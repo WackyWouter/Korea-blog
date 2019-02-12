cae2b558f6ee8273b4f3d125908a86b52f4b5eb1
WackyWouter <wfcbosch@gmail.com>
2019-02-12 03:48:51 +0100

    delete stuff in seeds

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -11,54 +11,6 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('comments')->insert(
-        [
-          [
-            'user_id' => 2,
-            'post_id' => 1,
-            'text' => 'Zo gaaf',
-          ],
-          [
-            'user_id' => 1,
-            'post_id' => 1,
-            'text' => 'Wat leuk',
-          ],
-          [
-            'user_id' => 1,
-            'post_id' => 1,
-            'text' => 'Heb je het naar je zin?',
-          ],
-          [
-            'user_id' => 2,
-            'post_id' => 2,
-            'text' => 'Zo gaaf',
-          ],
-          [
-            'user_id' => 1,
-            'post_id' => 2,
-            'text' => 'Wat leuk',
-          ],
-          [
-            'user_id' => 2,
-            'post_id' => 2,
-            'text' => 'Heb je het naar je zin?',
-          ],
-          [
-            'user_id' => 1,
-            'post_id' => 3,
-            'text' => 'Zo gaaf',
-          ],
-          [
-            'user_id' => 2,
-            'post_id' => 3,
-            'text' => 'Wat leuk',
-          ],
-          [
-            'user_id' => 1,
-            'post_id' => 3,
-            'text' => 'Heb je het naar je zin?',
-          ],
-        ]
-      );
+    
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,32 +17,12 @@ class UsersTableSeeder extends Seeder
       $role_viewer = Role::where('name', 'viewer')->first();
       $role_admin = Role::where('name', 'admin')->first();
 
-      $viewer = new User();
-      $viewer -> name = 'carina';
-      $viewer -> email = 'carina@gmail.com';
-      $viewer -> password = bcrypt('carina');
-      $viewer -> save();
-      $viewer -> roles() -> attach($role_viewer);
-
       $admin = new User();
-      $admin -> name = 'Wouter Bosch';
-      $admin -> email = 'wfcbosch@gmail.com';
+      $admin -> name = 'wouter';
+      $admin -> email = 'wouter@gmail.com';
       $admin -> password = bcrypt('wouter');
       $admin -> save();
       $admin -> roles() -> attach($role_admin);
 
-      $admin = new User();
-      $admin -> name = 'wouter2';
-      $admin -> email = 'wouter2@gmail.com';
-      $admin -> password = bcrypt('wouter');
-      $admin -> save();
-      $admin -> roles() -> attach($role_admin);
-
-      $viewer = new User();
-      $viewer -> name = 'carina2';
-      $viewer -> email = 'carina2@gmail.com';
-      $viewer -> password = bcrypt('carina');
-      $viewer -> save();
-      $viewer -> roles() -> attach($role_viewer);
     }
 }
